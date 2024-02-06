a31d524fb80d93fdf206c02186269215cbe9f845
MilanMareels <milan.mareels@gmail.com>
2024-02-06 17:56:23 +0100

    page change header

--- portfolio/src/pages/components/Header.tsx
+++ portfolio/src/pages/components/Header.tsx
@@ -4,7 +4,11 @@ import styles from "@/styles/Header.module.css"
 const Header = () => {
     return (
         <>
-            <header className={styles.header}></header>
+            <header className={styles.header}>
+                <section className={styles.textSection}>
+                    <p>Welkom op mijn blogwebsite! Hier deel ik mijn dagelijkse ervaringen en inzichten tijdens mijn stage. Als stagiair ben ik constant bezig met het verkennen van nieuwe taken, het opdoen van kennis en het uitbreiden van mijn vaardigheden. Deze blogs bieden een kijkje achter de schermen van mijn stage-avontuur, waarbij ik mijn uitdagingen, successen en leermomenten deel. Of het nu gaat om het assisteren van collega's, het bijwonen van vergaderingen of het werken aan projecten, ik neem je graag mee op mijn reis. Dus stap binnen, lees mee en laat je inspireren door mijn stage-avonturen!</p>
+                </section>
+            </header>
         </>
     )
 }
