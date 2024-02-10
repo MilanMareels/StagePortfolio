5d33e03774cf54da4ca18fba4f97ed1c656c5f06
MilanMareels <milan.mareels@gmail.com>
2024-02-10 10:34:22 +0100

    about page

--- portfolio/src/pages/components/Header.tsx
+++ portfolio/src/pages/components/Header.tsx
@@ -7,6 +7,10 @@ const Header = () => {
             <header className={styles.header}>
                 <section className={styles.textSection}>
                     <p>Welkom op mijn blogwebsite! Hier deel ik mijn dagelijkse ervaringen en inzichten tijdens mijn stage. Als stagiair ben ik constant bezig met het verkennen van nieuwe taken, het opdoen van kennis en het uitbreiden van mijn vaardigheden. Deze blogs bieden een kijkje achter de schermen van mijn stage-avontuur, waarbij ik mijn uitdagingen, successen en leermomenten deel. Of het nu gaat om het assisteren van collega's, het bijwonen van vergaderingen of het werken aan projecten, ik neem je graag mee op mijn reis. Dus stap binnen, lees mee en laat je inspireren door mijn stage-avonturen!</p>
+                    <p>
+                        Tijdens mijn stage bij B.ignited werk ik aan een boeiend project waarbij ik een tweedehands website moet ontwikkelen. Als onderdeel van dit project ben ik betrokken bij alle aspecten van het ontwikkelingsproces, van concept tot implementatie.
+
+                        Mijn verantwoordelijkheden omvatten het ontwerpen en bouwen van een gebruiksvriendelijke en aantrekkelijke website waar cronos gebruikers tweedehands producten kunnen kopen en verkopen. Ik werk samen met mijn collega's om de functionaliteiten van de website te definiëren en te implementeren, zoals het zoeken en filteren van producten op categorieën, prijs en locatie, het toevoegen en beheren van producten, en het faciliteren van communicatie tussen gebruikers.</p>
                 </section>
             </header>
         </>
